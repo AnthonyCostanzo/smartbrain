e1c8330601db9d376c81f1bac2d672c4f06d99e4
Anthony Costanzo <anthonycostanzo23@gmail.com>
2021-01-27 11:48:29 -0500

    aws-lambda-added

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
 const Rank = ({ name, entries }) => {
+  const [emoji,setEmoji] = useState("");
+
+  const generateEmoji = async(entries) => {
+    try {
+      const response = await fetch(`https://r90vhshe49.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`);
+      const data = await response.json();
+      setEmoji(data.input);
+    } catch(e) {
+      console.log(e);
+    }
+   
+  }
+  useEffect(() => {
+    generateEmoji(entries);
+  })
+
   return (
     <div>
       <div className='white f3'>
@@ -8,6 +24,9 @@ const Rank = ({ name, entries }) => {
       </div>
       <div className='white f1'>
         {entries}
+      </div>
+      <div className='white f3'>
+        {`Rank Badge : ${emoji}`}
       </div>
     </div>
   );
