1926187fecd97f625c0893db46ecf7b1aa9239e9
Anthony Costanzo <38962968+AnthonyCostanzo@users.noreply.github.com>
2021-08-17 17:38:55 -0400

    Add files via upload

---
+++ postgres/seed/seed.sql
@@ -0,0 +1,2 @@
+insert into users (name, email, entries, joined) values ('a', 'a@a.com', 5, '2018-01-01');
+insert into login (hash, email) values ('$2a$12$u5xBFzHTqPaZXSjVUyRiLOjpzQlhG9NReydTPT8C68ZlfHN6uHeo2', 'a@a.com');
